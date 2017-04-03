233e67e082610488d5cb4657d4346bc903cead8d
dmart331 <dmart331@gmail.com>
2017-04-03 12:20:32 -0500

    Added triggers to players

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,47 +7,68 @@ public class Ball : MonoBehaviour {
     public Vector3 initialImpulse;
     public Vector3 negitiveImpulse;
     public Rigidbody rb3D;
+    private bool isInPlayerTrigger = false;
+    private bool isStopped = false;
+    private string player;
+
     // Use this for initialization
     void Start () {
         rb3D = GetComponent<Rigidbody>();
         rb3D.AddForce(initialImpulse, ForceMode.Impulse);
 	}
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
-       //Freeze ball functions Below. Return is player right Tab is player left.
-            if (Input.GetKeyDown("return"))
-        {
-            //Stop moving
-            rb3D.isKinematic = true;
-            //Set parent to capsule
-            rb3D.transform.parent = GameObject.Find("PlayerLeft").transform;
-        }
-            if (Input.GetKeyUp("return")){
-            //Carry on
-            rb3D.isKinematic = false;
-            //Remove the parent
-            rb3D.transform.parent = null;
-            //Add the force
-            rb3D.AddForce(negitiveImpulse, ForceMode.Impulse);
-            
-            
+        if (Input.GetKeyDown("return")){
+            player = "left";
+            if (isInPlayerTrigger && player == "left") {
+                rb3D.isKinematic = true;
+                rb3D.transform.parent = GameObject.Find("PlayerLeft").transform;
+            }
         }
 
+        if (Input.GetKeyUp("return") && isStopped)
+        {
+            rb3D.isKinematic = false;
+            rb3D.transform.parent = null;
+            rb3D.AddForce(negitiveImpulse, ForceMode.Impulse);
+        }
 
         if (Input.GetKeyDown("tab"))
         {
-            rb3D.isKinematic = true;
-            rb3D.transform.parent = GameObject.Find("PlayerRight").transform;
+            player = "right";
+            if (isInPlayerTrigger && player == "right")
+            {
+                rb3D.isKinematic = true;
+                rb3D.transform.parent = GameObject.Find("PlayerRight").transform;
+            }
         }
-        if (Input.GetKeyUp("tab"))
+
+        if (Input.GetKeyUp("tab") && isStopped)
         {
             rb3D.isKinematic = false;
             rb3D.transform.parent = null;
             rb3D.AddForce(initialImpulse, ForceMode.Impulse);
         }
+
+
     }
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+
+            //Stop moving
+        isInPlayerTrigger = true;
+        isStopped = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInPlayerTrigger = false;
+        isStopped = false;
+    }
+
 }
 
 
